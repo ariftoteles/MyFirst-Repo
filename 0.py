e4737dcb5aecddc617559eb360b15e42fcc2152b
ariftoteles <arif.santoso4@gmail.com>
2020-02-13 10:53:24 +0700

    kedua

--- 0.py
+++ 0.py
@@ -4,5 +4,11 @@ print('x')
 print('y')
 
 # #bikin commit
-# 1. add files to staging area
+# 1. add files to staging areag
 # 2. git add . ==> menambhakan semua file
+# 3. git add 0a.py ==> menambahkan file tertentu
+# 4. git commit -m "Ini commit pertamaku"
+# 5. git log ==> 
+
+print('a')
+print('b')
