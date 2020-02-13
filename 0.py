26f311aa3e0831fb7cd138014965b64d0bc44933
ariftoteles <arif.santoso4@gmail.com>
2020-02-13 11:21:11 +0700

    ke5

--- 0.py
+++ 0.py
@@ -6,14 +6,14 @@ print('y')
 # #bikin commit
 # 1. add files to staging areag
 # 2. git add . ==> menambhakan semua file
-# 3. git add 0a.py ==> menambahkan file tertentu
+# 3. git add 0a.py ==> menambahkan file tertentu, tapi kalo udah add . ga perlu langkah ini
 # 4. git commit -m "Ini commit pertamaku"
 # 5. git log ==> cek log
 
 print('a')
 print('b')
 
-# CHECKOUT ==> read only ==> git log online ==> copy code lalu git chechkout (copy code tujuan commit) 
+# CHECKOUT ==> read only ==> git log --oneline ==> copy code lalu git chechkout (copy code tujuan commit) 
 # untuk kembali ke checkout master ==>> git checkout master
 # REVERT ==> back to past and modif commit
-# RESET ==> back to past and stay on there, not comeback to present
+# RESET ==> back to past and stay on there, not comeback to present ==>> git reset (code) DONT TRY THIS !!!
